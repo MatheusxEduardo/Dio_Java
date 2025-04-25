bd528d8ae57a1ab841cc5ef2247b8685c3cfc343
Matheus Eduardo <matheusxeduardox@outlook.com>
2025-04-24 22:52:15 -0300

    Adiciona .gitattributes para gerenciar finais de linha

---
+++ Spring/CadastroDeNinjas/src/test/java/dev/MatheusJava/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.MatheusJava.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
